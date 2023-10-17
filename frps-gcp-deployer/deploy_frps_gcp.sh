a7eaf63406c6b327d2346327cde07db7bffca7a3
Milkshak3s <justchris.vantine@gmail.com>
2023-10-17 15:47:02 -0700

    Add DNS record setting to frps deploy script

--- frps-gcp-deployer/deploy_frps_gcp.sh
+++ frps-gcp-deployer/deploy_frps_gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Must set:
-# GCP_PROJECT_ID, DNS_NAME, FRP_SERVER_PORT
+# GCP_PROJECT_ID, DNS_A_NAME, DNS_ZONE, DNS_BASE_DOMAIN, FRP_SERVER_PORT
 gcloud auth activate-service-account --key-file /etc/gcp-sa/service-account.json
 
 gcloud config set project $GCP_PROJECT_ID
@@ -10,9 +10,14 @@ gcloud config set compute/zone us-west1-b
 
 echo "PORT=${FRP_SERVER_PORT}" | cat - frps_startup.sh > temp && mv temp frps_startup.sh
 STARTUP_SCRIPT=`cat frps_startup.sh`
-gcloud compute instances create frps-${DNS_NAME} \
+INSTANCE_NAME="frps-${DNS_A_NAME}"
+gcloud compute instances create ${INSTANCE_NAME} \
     --image-family=debian-12 \
     --image-project=debian-cloud \
     --machine-type=e2-micro \
+    --network-tier=STANDARD \
     --metadata=startup-script=${STARTUP_SCRIPT}
 
+PROXY_IP=`gcloud compute instances describe ${INSTANCE_NAME} --format="value(networkInterfaces[0].accessConfigs[0].natIP)"`
+DNS_FULL_NAME="${DNS_ZONE}.${DNS_BASE_DOMAIN}"
+gcloud dns record-sets create ${DNS_A_NAME}.${DNS_FULL_NAME}. --rrdatas=${PROXY_IP} --type=A --ttl=60 --zone=${DNS_ZONE}
