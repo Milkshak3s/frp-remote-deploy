58e784701926190a40c97f971e623bf767b1abfc
Milkshak3s <justchris.vantine@gmail.com>
2023-10-18 14:38:41 -0700

    Remove unecessary mkdir

--- frps-gcp-deployer/frps_startup.sh
+++ frps-gcp-deployer/frps_startup.sh
@@ -1,7 +1,6 @@
 # an env var export is added to the top of this file during container runtime! see: deploy_frps_gcp.sh
 # PORT=7000
 wget -O /tmp/frp_0.52.1_linux_amd64.tar.gz https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_linux_amd64.tar.gz
-mkdir /tmp/frp
 tar -xf /tmp/frp_0.52.1_linux_amd64.tar.gz -C /tmp/
 echo "bindPort = ${PORT}" > /tmp/frp_0.52.1_linux_amd64/frps.toml
 
